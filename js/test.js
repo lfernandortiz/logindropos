5f933bf22310fee9e1b30ede3b27ef0b601c13fb
lfernandortiz <lfernandortiz@gmail.com>
2016-09-08 14:29:11 -0500

    Nuevo login base

---
+++ js/test.js
@@ -0,0 +1,10 @@
+
+window.addEventListener("load",
+						function(){
+							swal(
+								{   
+									title: "Error!",   
+									text: "Mensaje de error",   
+									type: "error",   
+									confirmButtonText: "Cool" });}, false);
+	
