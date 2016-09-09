f0dffaf016572bcad8186562f4060c831f169eda
lfernandortiz <lfernandortiz@gmail.com>
2016-09-09 12:04:44 -0500

    tamaño nombre app

--- js/test.js
+++ js/test.js
@@ -1,10 +1,16 @@
 
-window.addEventListener("load",
-						function(){
-							swal(
-								{   
-									title: "Error!",   
-									text: "Mensaje de error",   
-									type: "error",   
-									confirmButtonText: "Cool" });}, false);
-	
+function inicio(){
+	console.log("Registrando eventos....");
+	var user = document.getElementById("usuario");
+	user.addEventListener("focus", desenfocarFondo(), false);
+	var clave = document.getElementById("contrasenia");
+	clave.addEventListener("focus", desenfocarFondo(), false);
+}
+
+function desenfocarFondo(){
+	console.log("desenfocando..");
+	var fondo = document.getElementById("cb-slideshow");
+	fondo.setAttibutte("style", "filter: blur(5px)");
+}
+
+window.addEventListener("load", inicio(), false);
